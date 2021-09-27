4842c2e0295b7fed7eca80ca0242366e808539e0
Timothee <zingouingouin@hotmail.com>
2021-09-27 11:21:42 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'HowMuch',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
